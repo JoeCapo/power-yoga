f8632a48a30ffff173b788c419aeed4deda5069d
Joe <jrcaporiccio@gmail.com>
2022-11-08 17:47:45 -0500

    remove jquery and other sript files not needed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,18 +8,9 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <script type="text/javascript" src="js/jquery-1.8.3.min.js"></script>
-
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
-
-    <script type="text/javascript" src="js/jquery.mobile.customized.min.js"></script>
-    <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
-    <script type="text/javascript" src="js/camera.min.js"></script>
-    <script type="text/javascript" src="js/myscript.js"></script>
-    <script src="js/sorting.js" type="text/javascript"></script>
-    <script src="js/jquery.isotope.js" type="text/javascript"></script>
 
     @vite(['resources/scss/app.scss', 'resources/js/app.js'])
 </head>
