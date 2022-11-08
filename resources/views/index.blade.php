cbec911e93ca8d0c10ccf044a8cb1da97d4a1cac
Joe <jrcaporiccio@gmail.com>
2022-11-07 19:18:22 -0500

    change img-responsive to img-fluid

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -39,13 +39,13 @@
     <div class="container text-center">                    
         <div class="row team">                        
             <div class="col-12"> 
-                <img class="img-responsive" src="{{ Vite::asset('resources/images/debv.jpg') }}">                           
+                <img class="img-fluid" src="{{ Vite::asset('resources/images/debv.jpg') }}">                           
                 <h4>Debra Vieira</h4>
                 <h5>Yoga Instructor</h5>
                 <p>My name is Debra Vieria and I am someone who you might describe as an all-around athlete. Not a fantastic one, but someone who truly just loves the competition regardless. I have participated in endurance events such as marathons and IronMan Triathlons. I love all kinds of strength training including Crossfit. I actually taught myself long distance swimming to be able to participate in triathlons. I have participated and competed in it all. Over the years I practiced in a few types of yoga. I never felt that "connection" others spoke about. One day I was asked to meet for a power yoga class. Well the rest is history! After the first class I was hooked! I realized after that first class that I found what I was missing in other aspects of my training-the immediate connection between breath and athletic movements. From that point on, I was hooked on the satisfaction of getting stronger both mentally and physically, without a finish line in sight. After practicing yoga consistently, I decided to learn more and understand how the practice can be a guide for myself and my life. I discovered that what you learn on the mat, transfers off! How the connection between mind and body can be fostered and developed. I am a registered yoga teacher with my 200 hour yoga certification. I am continuing toward my 500 hour certification over the next year. In my teaching, I want to show others that it doesn't matter how strong, capable or trained you are -- yoga is all about what you put in and take out. I hope to inspire others to explore themselves, their mind, and body, in a way they haven't before.</p>
             </div>
             <div class="col-12"> 
-                <img class="img-responsive" src="{{ Vite::asset('resources/images/josh.jpg') }}">                           
+                <img class="img-fluid" src="{{ Vite::asset('resources/images/josh.jpg') }}">                           
                 <h4>Josh Kane</h4>
                 <h5>Yoga Instructor</h5>
                 <p>Josh Kane has been practicing yoga since 2002 and teaching since 2015. He received his 300hr Bhakti Yoga teacher training from Raghunath Cappo at ‘Supersoul Farm’ in Chatham, NY in 2018. He has also done several months of study at ‘The Esalen Institute’ in Big Sur, CA and used to play drums in a band called “Uh Huh Her”. You can follow him on IG @thedrumyogi</p>
